1a5c68932d10c3c93031d50cb9b16d8e049b3884
Ryan Dinh <ryan.dinh@hotmail.com>
2022-11-15 16:00:07 -0500

    GAMING
    We have gyro!!!!!

--- Assets/BallMOvement.cs
+++ Assets/BallMOvement.cs
@@ -6,33 +6,31 @@ public class BallMOvement : MonoBehaviour
 {
     Rigidbody body;
     public Vector2 input;
-    [SerializeField] float speed = 100;
+    [SerializeField] float speed = 1000;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        Input.gyro.enabled = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            input.y = 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            input.y = -1;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            input.x = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            input.x = -1;
-        }
+        Quaternion deviceRotation = Input.gyro.attitude;
+        //Debug.Log(deviceRotation);
+
+        //transform.rotation = deviceRotation;
+
+        Vector3 Device = deviceRotation.eulerAngles;
+        Debug.Log(Device);
+
+        //float Inputx = Mathf.Lerp(0f, 1f, Device.x / 90);
+        
+        input.y = Device.y - 180;
+        input.x = Device.x - 180;
+       
         Vector3 torque = new Vector3(input.x, 0, input.y);
         body.AddTorque(torque * speed * Time.fixedDeltaTime);
     }
